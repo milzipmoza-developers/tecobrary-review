8e8a4416d501eb22ab55c2f984a82a78a4b81533
thedevluffy <thedevluffy@gmail.com>
2021-11-13 21:16:17 +0900

    feat : 카테고리 색상을 구현한다.

---
+++ review-domain/src/main/kotlin/dev/milzipmoza/domain/category/color/CategoryColorOperationException.kt
@@ -0,0 +1,5 @@
+package dev.milzipmoza.domain.category.color
+
+import dev.milzipmoza.domain.book.BookOperationException
+
+class CategoryColorOperationException(message: String?) : BookOperationException(message)
---
+++ review-domain/src/test/java/dev/milzipmoza/domain/category/color/CategoryColorTest.kt
@@ -0,0 +1,39 @@
+package dev.milzipmoza.domain.category.color
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class CategoryColorTest {
+
+    @Test
+    fun `컬러 코드는 # 으로 시작하는 총 7글자로 설정할 수 있다`() {
+        assertDoesNotThrow {
+            CategoryColor("#000000")
+        }
+    }
+
+    @Test
+    fun `컬러 코드가 # 으로 시작하지 않으면 예외가 발생한다`() {
+        assertThrows<CategoryColorOperationException> {
+            CategoryColor("*0000000")
+        }
+    }
+
+    @Test
+    fun `컬러 코드가 7 글자가 아니면 예외가 발생한다`() {
+        assertThrows<CategoryColorOperationException> {
+            CategoryColor("#000")
+        }
+    }
+
+    @Test
+    fun `컬러 코드가 같으면 같은 색상으로 간주한다`() {
+        val it = CategoryColor("#121212")
+        val other = CategoryColor("#121212")
+
+        val sameAs = it.sameAs(other)
+
+        assertTrue(sameAs)
+    }
+}
