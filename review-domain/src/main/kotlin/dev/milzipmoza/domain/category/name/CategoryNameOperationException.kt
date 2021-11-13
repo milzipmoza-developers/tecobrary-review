9b80ed27d7482e8f0751e8e6ec48715d9a673fd4
thedevluffy <thedevluffy@gmail.com>
2021-11-13 21:31:49 +0900

    feat : 카테고리 이름을 구현한다.

---
+++ review-domain/src/main/kotlin/dev/milzipmoza/domain/category/name/CategoryNameOperationException.kt
@@ -0,0 +1,5 @@
+package dev.milzipmoza.domain.category.name
+
+import dev.milzipmoza.domain.book.BookOperationException
+
+class CategoryNameOperationException(message: String?) : BookOperationException(message)
---
+++ review-domain/src/test/java/dev/milzipmoza/domain/category/name/CategoryNameTest.kt
@@ -0,0 +1,32 @@
+package dev.milzipmoza.domain.category.name
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+internal class CategoryNameTest {
+
+    @Test
+    fun `이름이 두글자 미만이면 예외가 발생한다`() {
+        assertThrows<CategoryNameOperationException> {
+            CategoryName("1")
+        }
+    }
+
+    @Test
+    fun `이름이 10자를 초과하면 예외가 발생한다`() {
+        assertThrows<CategoryNameOperationException> {
+            CategoryName("01234567891")
+        }
+    }
+
+    @Test
+    fun `이름이 같으면 같은 것으로 간주한다`() {
+        val it = CategoryName("객체지향")
+        val other = CategoryName("객체지향")
+
+        val sameAs = it.sameAs(other)
+
+        assertTrue(sameAs)
+    }
+}
