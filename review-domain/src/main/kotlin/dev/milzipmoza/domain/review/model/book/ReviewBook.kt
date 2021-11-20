a71d859999ac92363f300f157304b3c70e060ba8
thedevluffy <thedevluffy@gmail.com>
2021-11-21 00:19:20 +0900

    feat : 리뷰 등록자와 리뷰 도서를 구현한다.

---
+++ review-domain/src/main/kotlin/dev/milzipmoza/domain/review/model/book/ReviewBook.kt
@@ -0,0 +1,32 @@
+package dev.milzipmoza.domain.review.model.book
+
+import dev.milzipmoza.domain.Value
+
+class ReviewBook(
+        val no: String,
+        val title: String,
+        val image: String
+) : Value<ReviewBook> {
+
+    override fun sameAs(other: ReviewBook): Boolean = this == other
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ReviewBook
+
+        if (no != other.no) return false
+        if (title != other.title) return false
+        if (image != other.image) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = no.hashCode()
+        result = 31 * result + title.hashCode()
+        result = 31 * result + image.hashCode()
+        return result
+    }
+}
---
+++ review-domain/src/main/kotlin/dev/milzipmoza/domain/review/model/member/ReviewMember.kt
@@ -0,0 +1,8 @@
+package dev.milzipmoza.domain.review.model.member
+
+import dev.milzipmoza.domain.Value
+
+class ReviewMember(
+        val no: String,
+        val name: String
+)
