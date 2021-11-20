546c53561b34733a31c344cee2738895ec40f1c8
thedevluffy <thedevluffy@gmail.com>
2021-11-21 00:23:42 +0900

    refactor : 페이지 엔티티 리팩토링

--- review-domain/src/main/kotlin/dev/milzipmoza/domain/PageEntities.kt
+++ review-domain/src/main/kotlin/dev/milzipmoza/domain/PageEntities.kt
@@ -1,8 +1,10 @@
 package dev.milzipmoza.domain
 
-data class PageEntities<T>(
+data class PageEntities<T : Entity<Any>>(
         val total: Long,
         val start: Long,
         val size: Long,
+        val isFirst: Boolean,
+        val isLast: Boolean,
         val items: List<T>
 )
