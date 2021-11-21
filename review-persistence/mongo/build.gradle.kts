e967468725ef976105cc0dbc062937b0ff493e42
thedevluffy <thedevluffy@gmail.com>
2021-11-21 13:36:34 +0900

    chore : mongo 모듈 mongo 관련 의존성을 추가한다

--- review-persistence/mongo/build.gradle.kts
+++ review-persistence/mongo/build.gradle.kts
@@ -6,5 +6,10 @@ plugins {
 }
 
 dependencies {
+	implementation(project(":review-domain"))
+
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
 }
