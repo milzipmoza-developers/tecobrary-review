e6a649b53e1328196454b4cd3130444ea99d0abd
thedevluffy <thedevluffy@gmail.com>
2021-11-22 00:36:29 +0900

    test : 테스트용 embedded mongo 를 세팅한다.

---
+++ review-persistence/mongo/src/test/kotlin/dev/milzipmoza/review/MongoTestApplication.kt
@@ -0,0 +1,11 @@
+package dev.milzipmoza.review
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Configuration
+
+@SpringBootApplication
+class MongoTestApplication {
+
+}
