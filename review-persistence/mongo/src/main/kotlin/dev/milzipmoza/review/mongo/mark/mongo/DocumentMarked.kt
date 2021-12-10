a279b5c67beb35fe3bb919a383fd260cd2eef46c
thedevluffy <thedevluffy@gmail.com>
2021-12-10 17:11:26 +0900

    feat : 낙관적 락을 위한 DocumentMarked 필드를 추가한다.

--- review-persistence/mongo/src/main/kotlin/dev/milzipmoza/review/mongo/mark/mongo/DocumentMarked.kt
+++ review-persistence/mongo/src/main/kotlin/dev/milzipmoza/review/mongo/mark/mongo/DocumentMarked.kt
@@ -4,10 +4,12 @@ import org.bson.types.ObjectId
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
+import org.springframework.data.annotation.Version
 
 @Document(collection = "marked_marks")
 data class DocumentMarked(
         @Id val id: ObjectId = ObjectId.get(),
         var marked: Boolean,
-        var txDateTime: LocalDateTime
+        var txDateTime: LocalDateTime,
+        @Version private var version: Long = 0
 )
