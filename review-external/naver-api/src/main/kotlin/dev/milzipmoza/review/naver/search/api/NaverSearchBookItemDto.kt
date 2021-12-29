79cac2b385c3fb2e689129de970112f6afbc9867
thedevluffy <thedevluffy@gmail.com>
2021-12-30 00:08:13 +0900

    feat: search books 구현체 naver api 로그를 추가한다.

--- review-external/naver-api/src/main/kotlin/dev/milzipmoza/review/naver/search/api/NaverSearchBookItemDto.kt
+++ review-external/naver-api/src/main/kotlin/dev/milzipmoza/review/naver/search/api/NaverSearchBookItemDto.kt
@@ -4,11 +4,13 @@ data class NaverSearchBookItemDto(
         val title: String = "",
         val link: String = "",
         val image: String = "",
-        val price: Int = 0,
-        val discount: Int = 0,
         val author: String = "",
         val publisher: String = "",
         val pubdate: String = "",
         val isbn: String = "",
         val description: String = ""
-)
+) {
+    override fun toString(): String {
+        return "NaverSearchBookItemDto(\ntitle='$title',\n link='$link',\n image='$image',\n author='$author',\n publisher='$publisher',\n pubdate='$pubdate',\n isbn='$isbn',\n description='$description'\n)"
+    }
+}
--- review-external/naver-api/src/main/kotlin/dev/milzipmoza/review/naver/search/domain/NaverSearchBooks.kt
+++ review-external/naver-api/src/main/kotlin/dev/milzipmoza/review/naver/search/domain/NaverSearchBooks.kt
@@ -5,12 +5,15 @@ import dev.milzipmoza.review.domain.PageQuery
 import dev.milzipmoza.review.domain.search.SearchBooks
 import dev.milzipmoza.review.domain.search.model.SearchBook
 import dev.milzipmoza.review.naver.search.api.NaverApiClient
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Repository
 
 @Repository
 class NaverSearchBooks(
         private val naverApiClient: NaverApiClient
 ) : SearchBooks {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
 
     override fun findAllBy(keyword: String, pageQuery: PageQuery): PageEntities<SearchBook> {
         val response = naverApiClient.searchBooks(
@@ -19,12 +22,17 @@ class NaverSearchBooks(
                 start = pageQuery.start
         )
 
+        response.items.forEach {
+            logger.info("naver api item={}", it)
+        }
+
         return PageEntities(
                 total = response.total,
                 size = response.display,
                 isFirst = false,
                 isLast = false,
                 items = response.items
+                        .filter { it.image.isNotBlank() }
                         .map { NaverSearchBookMapper.map(it) }
                         .toList()
         )
