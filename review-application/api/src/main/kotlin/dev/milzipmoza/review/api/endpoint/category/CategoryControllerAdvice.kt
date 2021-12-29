7674f9de8b16b470cb6a4a8d1df74d86f483abfc
thedevluffy <thedevluffy@gmail.com>
2021-12-30 00:10:40 +0900

    refactor: CategoryActionControllerAdvice 를 category 관련 controller 에 대해서만 동작하도록 수정한다.

--- review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/category/CategoryControllerAdvice.kt
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/category/CategoryControllerAdvice.kt
@@ -1,6 +1,9 @@
-package dev.milzipmoza.review.api.endpoint.category.action
+package dev.milzipmoza.review.api.endpoint.category
 
 import dev.milzipmoza.review.api.ApiResponse
+import dev.milzipmoza.review.api.endpoint.category.action.CategoryCreateController
+import dev.milzipmoza.review.api.endpoint.category.action.CategoryUpdateController
+import dev.milzipmoza.review.api.endpoint.category.search.CategoryController
 import dev.milzipmoza.review.domain.category.CategoryOperationException
 import dev.milzipmoza.review.mongo.extension.Logger
 import org.springframework.http.HttpStatus
@@ -8,8 +11,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
-@RestControllerAdvice
-class CategoryActionControllerAdvice {
+@RestControllerAdvice(basePackageClasses = [
+    CategoryCreateController::class,
+    CategoryUpdateController::class,
+    CategoryController::class
+])
+class CategoryControllerAdvice {
 
     private val logger = Logger.of(this)
 
@@ -17,13 +24,13 @@ class CategoryActionControllerAdvice {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handle(e: CategoryOperationException): ApiResponse<Nothing> {
         logger.warn("카테고리 예외 발생", e)
-        return ApiResponse.error(e.message ?: "카테고리 연산 수행 중 에러가 발생하였습니다.")
+        return ApiResponse.error(e.message ?: "카테고리 작업 중 에러가 발생하였습니다.")
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     fun handle(e: IllegalArgumentException): ApiResponse<Nothing> {
         logger.warn("카테고리 예외 발생", e)
-        return ApiResponse.error(e.message ?: "카테고리 연산 수행 중 에러가 발생하였습니다.")
+        return ApiResponse.error(e.message ?: "카테고리 작업 중 에러가 발생하였습니다.")
     }
 }
