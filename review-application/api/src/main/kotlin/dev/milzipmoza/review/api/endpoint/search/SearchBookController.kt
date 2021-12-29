e8a806af9addba72219c27bf6a5effd706598b32
thedevluffy <thedevluffy@gmail.com>
2021-12-29 23:16:27 +0900

    refactor: 도서 검색 controller 를 리팩토링한다.

--- review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/search/SearchBookController.kt
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/search/SearchBookController.kt
@@ -2,28 +2,17 @@ package dev.milzipmoza.review.api.endpoint.search
 
 import dev.milzipmoza.review.api.ApiResponse
 import dev.milzipmoza.review.api.PageData
-import dev.milzipmoza.review.domain.PageQuery
-import dev.milzipmoza.review.domain.search.SearchBooks
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class SearchBookController(
-        private val searchBooks: SearchBooks
+        private val searchBookService: SearchBookService
 ) {
 
     @GetMapping("/api/search-books")
-    fun search(pageParam: SearchBookPageRequest): ApiResponse<PageData<SearchBookElementDto>> {
-        val pageSearchResult = searchBooks.findAllBy(pageParam.keyword, PageQuery(pageParam.page, pageParam.size))
-
-        return ApiResponse.success(data = PageData(
-                total = pageSearchResult.total,
-                size = pageSearchResult.size,
-                isFirst = pageSearchResult.isFirst,
-                isLast = pageSearchResult.isLast,
-                items = pageSearchResult.items
-                        .map { SearchBookElementDto.of(it) }
-                        .toList()
-        ))
+    fun search(searchRequest: SearchBookPageRequest): ApiResponse<PageData<SearchBookElementDto>> {
+        val pageData = searchBookService.search(searchRequest.keyword, searchRequest.page, searchRequest.size)
+        return ApiResponse.success(data = pageData)
     }
 }
---
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/search/SearchBookService.kt
@@ -0,0 +1,23 @@
+package dev.milzipmoza.review.api.endpoint.search
+
+import dev.milzipmoza.review.annotation.ApplicationService
+import dev.milzipmoza.review.api.PageData
+import dev.milzipmoza.review.domain.PageQuery
+import dev.milzipmoza.review.domain.search.SearchBooks
+
+@ApplicationService
+class SearchBookService(
+        private val searchBooks: SearchBooks
+) {
+
+    fun search(keyword: String, page: Int, size: Int): PageData<SearchBookElementDto> {
+        if (keyword.length < 2) {
+            throw IllegalArgumentException("검색어를 두 글자 이상 입력해주세요.")
+        }
+        if (page < 1) {
+            throw IllegalArgumentException("페이지는 1부터 시작하는 값 입니다.")
+        }
+        val pageEntities = searchBooks.findAllBy(keyword, PageQuery(page, size))
+        return PageData.of(pageEntities, SearchBookElementDto::of)
+    }
+}
