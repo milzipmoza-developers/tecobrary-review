f505b136ae55ecf92ea8674f260f9ef1b0193133
thedevluffy <thedevluffy@gmail.com>
2022-01-21 21:39:07 +0900

    chore: jaxb-api 의존성을 추가한다.

--- review-application/api/build.gradle.kts
+++ review-application/api/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
+	implementation("javax.xml.bind:jaxb-api:2.3.1")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-webflux")
