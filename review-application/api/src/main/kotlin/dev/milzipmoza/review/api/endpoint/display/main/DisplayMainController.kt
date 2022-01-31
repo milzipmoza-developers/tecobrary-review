dd3984349cce44f033f010a4fcc5ec394d3c81bb
thedevluffy <thedevluffy@gmail.com>
2022-01-31 18:47:43 +0900

    feat: DisplayMainController 를 분리한다.

--- review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/display/main/DisplayMainController.kt
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/display/main/DisplayMainController.kt
@@ -1,18 +1,27 @@
 package dev.milzipmoza.review.api.endpoint.display.main
 
 import dev.milzipmoza.review.api.ApiResponse
+import dev.milzipmoza.review.api.endpoint.display.main.dto.DisplayMainCategoryDto
 import dev.milzipmoza.review.api.endpoint.display.main.dto.DisplayMainDto
+import dev.milzipmoza.review.api.endpoint.display.main.dto.DisplayMainNewBookSectionDto
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class DisplayMainController(
-        private val displayMainFacade: DisplayMainFacade
+        private val displayMainNewBookService: DisplayMainNewBookService,
+        private val displayMainCategoryService: DisplayMainCategoryService
 ) {
 
-    @GetMapping("/api/main")
-    fun get(): ApiResponse<DisplayMainDto> {
-        val displayMain = displayMainFacade.get()
-        return ApiResponse.success(data = displayMain)
+    @GetMapping("/api/display/main/new-books")
+    fun getNewBooks(): ApiResponse<DisplayMainNewBookSectionDto> {
+        val displayNewBooks = displayMainNewBookService.getRecentPublished()
+        return ApiResponse.success(data = displayNewBooks)
+    }
+
+    @GetMapping("/api/display/main/categories")
+    fun getCategories(): ApiResponse<List<DisplayMainCategoryDto>> {
+        val displayRandomCategories = displayMainCategoryService.getRandomCategories()
+        return ApiResponse.success(data = displayRandomCategories)
     }
 }
