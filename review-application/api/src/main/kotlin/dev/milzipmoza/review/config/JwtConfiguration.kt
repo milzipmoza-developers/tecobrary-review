d632ab85200ac3b080c819b2d4cc4a4363fdafa6
thedevluffy <thedevluffy@gmail.com>
2022-02-05 23:44:06 +0900

    feat : 리뷰 관련 endpoint 를 filter 에 등록한다.

--- review-application/api/src/main/kotlin/dev/milzipmoza/review/config/JwtConfiguration.kt
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/config/JwtConfiguration.kt
@@ -25,5 +25,6 @@ class JwtConfiguration {
                     .apply {
                         this.filter = optionalJwtAuthenticationFilter
                         addUrlPatterns("/api/displays/books/*")
+                        addUrlPatterns("/api/reviews/**")
                     }
 }
