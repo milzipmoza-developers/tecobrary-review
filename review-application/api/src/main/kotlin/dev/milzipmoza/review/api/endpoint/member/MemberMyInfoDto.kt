261a5b7fe3d80a1ca3267fb59c8ed0ffe29945ce
thedevluffy <thedevluffy@gmail.com>
2022-02-21 22:15:25 +0900

    (#3) feat : 내정보 조회 api 에 최근 북마크 3개를 추가한다.

--- review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/member/MemberMyInfoDto.kt
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/member/MemberMyInfoDto.kt
@@ -1,7 +1,11 @@
 package dev.milzipmoza.review.api.endpoint.member
 
+import com.fasterxml.jackson.annotation.JsonFormat
+import java.time.LocalDateTime
+
 data class MemberMyInfoDto(
-        val member: MemberDto
+        val member: MemberDto,
+        val bookmarks: List<BookmarkDto>
 )
 
 data class MemberDto(
@@ -10,4 +14,18 @@ data class MemberDto(
         val profileImageUrl: String,
         val blogUrl: String,
         val description: String
+)
+
+data class BookmarkDto(
+        val isbn: String,
+        val title: String,
+        val imageUrl: String,
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSS")
+        val markDateTime: LocalDateTime,
+        val tags: List<BookmarkTagDto>
+)
+
+data class BookmarkTagDto(
+        val name: String,
+        val colorCode: String
 )
--- review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/member/MemberMyInfoService.kt
+++ review-application/api/src/main/kotlin/dev/milzipmoza/review/api/endpoint/member/MemberMyInfoService.kt
@@ -1,16 +1,25 @@
 package dev.milzipmoza.review.api.endpoint.member
 
 import dev.milzipmoza.review.annotation.ApplicationService
+import dev.milzipmoza.review.domain.book.Books
+import dev.milzipmoza.review.domain.mark.*
 import dev.milzipmoza.review.domain.member.Members
 
 @ApplicationService
 class MemberMyInfoService(
-        private val members: Members
+        private val members: Members,
+        private val bookmarks: Bookmarks,
+        private val books: Books
 ) {
 
     fun get(memberNo: String): MemberMyInfoDto {
 
         val member = members.findBy(memberNo)
+
+        val bookmarks: List<Bookmark> = bookmarks.getRecentBookmarkAfter(MarkMember(member.no), 3)
+
+        val bookIsbns = bookmarks.map { it.book.no }
+        val books = books.findAllIn(bookIsbns)
 
         return MemberMyInfoDto(
                 member = MemberDto(
@@ -19,7 +28,17 @@ class MemberMyInfoService(
                         profileImageUrl = member.info.profileImageUrl,
                         blogUrl = member.info.blogUrl,
                         description = member.info.description
-                )
+                ),
+                bookmarks = bookmarks.map { bookmark ->
+                    val book = books.find { it.isbn == bookmark.book.no }!!
+                    BookmarkDto(
+                            isbn = book.isbn,
+                            title = book.detail.summarizedTitle,
+                            imageUrl = book.detail.fullImageUrl,
+                            markDateTime = bookmark.markDateTime,
+                            tags = book.tags.map { BookmarkTagDto(it.name, it.colorCode) }
+                    )
+                }
         )
     }
 }
